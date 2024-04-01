dca81eb7bde465ad3e9ac2d23de512b7d6e2e28e
yehuda24 <158982057+yehuda24@users.noreply.github.com>
2024-04-01 12:37:41 +0700

    Update 2024.4.1
    - Fix bug on the ball bounce

--- Assets/Script/BallControl.cs
+++ Assets/Script/BallControl.cs
@@ -17,11 +17,11 @@ public class BallControl : MonoBehaviour
         rand = Random.Range(0, 2);
         if (rand == 0)
         {
-            rb2d.AddForce(new Vector2(40, -22));
+            rb2d.AddForce(new Vector2(10, -4));
         }
         else
         {
-            rb2d.AddForce(new Vector2(-40, -22));
+            rb2d.AddForce(new Vector2(-10, -4));
         }
     }
 
@@ -36,4 +36,28 @@ public class BallControl : MonoBehaviour
         rb2d.velocity = Vector2.zero;
         transform.position = Vector2.zero;
     }
+
+    [SerializeField] private int stageCollisionCount;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            rb2d.AddForce(new Vector2(10, -4));
+            stageCollisionCount = 0;
+        } else if(collision.gameObject.name == "Bear")
+        {
+            rb2d.AddForce(new Vector2(-10, -4));
+            stageCollisionCount = 0;
+        }
+        else 
+        {
+            stageCollisionCount++;
+            Debug.Log("Stage Collision = " + stageCollisionCount);
+            if(stageCollisionCount > 6)
+            {
+                GoBall();
+            }
+        }
+    }
+
 }
